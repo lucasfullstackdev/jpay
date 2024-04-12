fc58052949ab7efff3a7696e06e4dbd02568d503
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-12 10:12:43 -0300

    feat: extraindo responsabilidade de criacao do documentos

--- app/Jobs/CreateDocumentJob.php
+++ app/Jobs/CreateDocumentJob.php
@@ -2,31 +2,25 @@
 
 namespace App\Jobs;
 
-use App\Exceptions\CreateException;
-use App\Models\Customer;
-use App\Models\Document;
-use App\Services\Signature\ClickSign\ClickSignDocument;
-use App\Services\Signature\SignatureService;
+use App\Services\DocumentService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
 
 class CreateDocumentJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private SignatureService $signatureService;
-    private Customer $customer;
+    private DocumentService $documentService;
 
     /**
      * Create a new job instance.
      */
     public function __construct(public object $payment)
     {
-        $this->signatureService = new SignatureService();
+        $this->documentService = app(DocumentService::class);
     }
 
     /**
@@ -34,47 +28,13 @@ class CreateDocumentJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $this->customer = $this->getCustomer();
-        /** Se nao encontrar o customer, barrar */
-        if (empty($this->customer)) {
-            return;
-        }
-
-        /** Se encontrar documento, barrar */
-        if ($this->hasDocument()) {
-            return;
-        }
-
-        // Envia o documento para a ClickSign
-        $document = $this->sendDocument();
-
-        // Cria o documento no banco de dados
-        try {
-            DB::beginTransaction();
-            $document = Document::create($document);
-            DB::commit();
-        } catch (\Throwable $th) {
-            throw new CreateException('Erro ao salvar Documento no Banco de Dados', $th->getMessage());
-        }
-
         // Após criar o documento, dispara o job para criar o signer
+        $document = $this->documentService->createDocument($this->payment);
+
+        if (empty($document)) {
+            return;
+        }
+
         CreateSignerJob::dispatch($document);
-    }
-
-    private function getCustomer()
-    {
-        return Customer::with('company')->where('sku', $this->payment->customer)->first();
-    }
-
-    private function hasDocument(): bool
-    {
-        return (bool) Document::where('customer', $this->customer->sku)->first() ?? null;
-    }
-
-    private function sendDocument()
-    {
-        return (array) $this->signatureService->sendDocument(
-            new ClickSignDocument($this->customer)
-        );
     }
 }
---
+++ app/Services/DocumentService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\CreateException;
+use App\Models\Customer;
+use App\Models\Document;
+use App\Services\Signature\ClickSign\ClickSignDocument;
+use App\Services\Signature\SignatureService;
+use Illuminate\Support\Facades\DB;
+
+class DocumentService
+{
+  public function __construct(private SignatureService $signatureService)
+  {
+  }
+
+  public function createDocument(object $payment)
+  {
+    $customer = $this->getCustomer($payment);
+    /** Se nao encontrar o customer, barrar */
+    if (empty($customer)) {
+      return;
+    }
+
+    /** Se encontrar documento, barrar */
+    // if ($this->hasDocument($customer)) {
+    //   return;
+    // }
+
+    // Envia o documento para a ClickSign
+    $document = $this->sendDocument($customer);
+
+    // Cria o documento no banco de dados
+    try {
+      DB::beginTransaction();
+      $document = Document::create($document);
+      DB::commit();
+
+      return $document;
+    } catch (\Throwable $th) {
+      throw new CreateException('Erro ao salvar Documento no Banco de Dados', $th->getMessage());
+    }
+  }
+
+  private function getCustomer(object $payment)
+  {
+    return Customer::with('company')->where('sku', $payment->customer)->first();
+  }
+
+  private function hasDocument(Customer $customer): bool
+  {
+    return (bool) Document::where('customer', $customer->sku)->first() ?? null;
+  }
+
+  private function sendDocument(Customer $customer)
+  {
+    return (array) $this->signatureService->sendDocument(
+      new ClickSignDocument($customer)
+    );
+  }
+}
