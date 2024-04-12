55b605032ee9d8a3435d509abecf02edbbd318da
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-12 09:18:15 -0300

    feat: extraindo responsabilidade de criacao do cliente para o CustomerService

--- app/Jobs/CreateExternalCustomerJob.php
+++ app/Jobs/CreateExternalCustomerJob.php
@@ -2,36 +2,26 @@
 
 namespace App\Jobs;
 
-use App\Dtos\CompanyOshi;
-use App\Dtos\Customer\CustomerOshi;
-use App\Exceptions\CreateException;
-use App\Exceptions\UpdateException;
-use App\Http\Requests\Purchase;
-use App\Models\Company;
-use App\Models\Customer;
 use App\Services\Banking\Asaas\AsaasBilling;
-use App\Services\Banking\Asaas\AsaasCustomer;
-use App\Services\Banking\BankingService;
+use App\Services\CustomerService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class CreateExternalCustomerJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private BankingService $bankingService;
+    private CustomerService $customerService;
 
     /**
      * Create a new job instance.
      */
     public function __construct(public $purchase)
     {
-        $this->bankingService = new BankingService();
+        $this->customerService = app(CustomerService::class);
     }
 
     /**
@@ -40,39 +30,10 @@ class CreateExternalCustomerJob implements ShouldQueue
     public function handle(): void
     {
         /** Enviando o cliente para o ASAAS */
-        $customerToUpdate = $this->sendCustomer($this->purchase);
-
-        /**
-         * Só cadastrar o cliente e a empresa se conseguirmos enviar o cliente para o ASAAS
-         */
-        try {
-            DB::beginTransaction();
-
-            $customerOshi = new CustomerOshi((object) $this->purchase);
-
-            // Cadastro do cliente
-            $customerOshi->sku = $customerToUpdate->sku;
-            $customer = Customer::create((array) $customerOshi);
-
-            // Cadastrando a empresa
-            $customerOshi->company->owner_id = $customer->id;
-            Company::create((array) $customerOshi->company);
-
-            DB::commit();
-        } catch (\Throwable $th) {
-            DB::rollBack();
-            throw new CreateException('Erro ao criar o cliente', $th->getMessage());
-        }
+        $customer = $this->customerService->createExternalCustomer($this->purchase);
 
         CreateBillingJob::dispatch(
             new AsaasBilling($customer)
-        );
-    }
-
-    private function sendCustomer($purchase): object
-    {
-        return $this->bankingService->createCustomer(
-            new AsaasCustomer((object) $purchase)
         );
     }
 }
---
+++ app/Services/CustomerService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\Dtos\Customer\CustomerOshi;
+use App\Exceptions\CreateException;
+use App\Models\Company;
+use App\Models\Customer;
+use App\Services\Banking\Asaas\AsaasCustomer;
+use App\Services\Banking\BankingService;
+use Illuminate\Support\Facades\DB;
+
+class CustomerService
+{
+
+  public function __construct(private BankingService $bankingService)
+  {
+  }
+
+  public function createExternalCustomer($purchase): Customer
+  {
+    $customerToUpdate = $this->sendCustomer($purchase);
+
+    /**
+     * Só cadastrar o cliente e a empresa se conseguirmos enviar o cliente para o ASAAS
+     */
+    try {
+      DB::beginTransaction();
+
+      $customerOshi = new CustomerOshi((object) $purchase);
+
+      // Cadastro do cliente
+      $customerOshi->sku = $customerToUpdate->sku;
+      $customer = Customer::create((array) $customerOshi);
+
+      // Cadastrando a empresa
+      $customerOshi->company->owner_id = $customer->id;
+      Company::create((array) $customerOshi->company);
+
+      DB::commit();
+
+      return $customer;
+    } catch (\Throwable $th) {
+      DB::rollBack();
+      throw new CreateException('Erro ao criar o cliente', $th->getMessage());
+    }
+  }
+
+  private function sendCustomer($purchase): object
+  {
+    return $this->bankingService->createCustomer(
+      new AsaasCustomer((object) $purchase)
+    );
+  }
+}
