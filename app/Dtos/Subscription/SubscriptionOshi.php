648a4d0781123e24eb8670de114ae55770657ab1
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-27 19:13:54 -0300

    feat: implementando tratativa ao gerar o objeto necessario para salvar a subscription no banco de dados

--- app/Dtos/Subscription/SubscriptionOshi.php
+++ app/Dtos/Subscription/SubscriptionOshi.php
@@ -2,6 +2,7 @@
 
 namespace App\Dtos\Subscription;
 
+use App\Exceptions\SubscriptionException;
 use App\Services\Banking\CustomerInterface;
 
 class SubscriptionOshi implements CustomerInterface
@@ -15,11 +16,15 @@ class SubscriptionOshi implements CustomerInterface
 
   public function __construct(object $subscription)
   {
-    $this->sku = $subscription->id;
-    $this->customer = $subscription->customer;
-    $this->value = $subscription->value;
-    $this->cycle = $subscription->cycle;
-    $this->description = $subscription->description;
-    $this->subscription = json_encode($subscription);
+    try {
+      $this->sku = $subscription->id;
+      $this->customer = $subscription->customer;
+      $this->value = $subscription->value;
+      $this->cycle = $subscription->cycle;
+      $this->description = $subscription->description;
+      $this->subscription = json_encode($subscription);
+    } catch (\Throwable $th) {
+      throw new SubscriptionException('Erro ao criar a estrutura de dados para salvar a Subscription no banco de dados',  $th->getMessage(), (array) $subscription);
+    }
   }
 }
