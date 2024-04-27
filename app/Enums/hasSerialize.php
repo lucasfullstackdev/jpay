a5a4145c4de71526898319648f806083e46b8d80
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-27 16:57:55 -0300

    feat: implementando metodo responsavel por buscar valor dinamicamente do enum que implementar a trait hasSerialize

--- app/Enums/hasSerialize.php
+++ app/Enums/hasSerialize.php
@@ -8,7 +8,17 @@ trait hasSerialize
 {
   public static function getValues(): array
   {
-    $reflection = new ReflectionClass(__CLASS__);
-    return array_values($reflection->getConstants());
+    return (new ReflectionClass(__CLASS__))->getConstants();
+  }
+
+  public static function getValue(string $key): int|string
+  {
+    $values = self::getValues();
+
+    if (!array_key_exists($key, $values)) {
+      throw new \InvalidArgumentException("Chave não encontrada");
+    }
+
+    return $values[$key]->value;
   }
 }
