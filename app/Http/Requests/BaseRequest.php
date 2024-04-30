af7555af614c7c618f930037680995a8c6e18020
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-29 23:17:43 -0300

    feat: somente requisicoes recebidas da LP, que falharam e que possuem email valido serao enviadas para Discord

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -7,6 +7,7 @@ use App\Jobs\SendErrorToDiscordJob;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 use Symfony\Component\HttpFoundation\Response;
 
 class BaseRequest extends FormRequest
@@ -21,7 +22,36 @@ class BaseRequest extends FormRequest
 
   protected function failedValidation(Validator $validator)
   {
-    // Envia o erro para o Discord
+    /**
+     * Verifica se o email é válido e envia o erro para o Discord.
+     * Essa verificação é necessária para evitar que o Discord seja inundado com mensagens de erro, afinal
+     * o canal para captura dos erros da Landing Page será utilizado para recuperação de vendas.
+     */
+    if ($this->emailIsValid()) {
+      $this->sendErrorToDiscord($validator);
+    }
+
+    throw new HttpResponseException(response()->json([
+      'success' => false,
+      'errors'  => $validator->errors(),
+      'data'    => []
+    ], Response::HTTP_BAD_REQUEST));
+  }
+
+  private function emailIsValid(): bool
+  {
+    $validator = FacadesValidator::make(['email' => $this->customer['email'] ?? null], [
+      'email' => 'email:rfc,dns'
+    ]);
+
+    return !$validator->fails();
+  }
+
+  /**
+   * Envio de erro para o Discord
+   */
+  private function sendErrorToDiscord($validator)
+  {
     SendErrorToDiscordJob::dispatch(
       new DiscordMessage('Erro nos dados recebidos pela Landing Page', Response::HTTP_BAD_REQUEST, [
         'payload' => $this->all(),
@@ -29,11 +59,5 @@ class BaseRequest extends FormRequest
       ]),
       env('DISCORD_WEBHOOK_URL_LANDING_PAGE_ERRORS')
     );
-
-    throw new HttpResponseException(response()->json([
-      'success' => false,
-      'errors'  => $validator->errors(),
-      'data'    => []
-    ], Response::HTTP_BAD_REQUEST));
   }
 }
