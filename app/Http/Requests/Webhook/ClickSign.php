8a59ea3894526733274e0640d57fd1bbacb91dac
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-05-04 15:32:31 -0300

    feat: adicionando registro de logs para debug

--- app/Http/Requests/Webhook/ClickSign.php
+++ app/Http/Requests/Webhook/ClickSign.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests\Webhook;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClickSign extends FormRequest
 {
@@ -20,12 +24,12 @@ class ClickSign extends FormRequest
         if ($this->secret !== env('CLICKSIGN_WEBHOOK_SECRET')) {
             return false;
         }
-        
+
         /* Validar se o evento está presente */
         if (empty($this->event)) {
             return false;
         }
-        
+
         /* Validar se o evento é válido */
         if (empty($this->event['name'])) {
             return false;
@@ -36,13 +40,28 @@ class ClickSign extends FormRequest
 
     public function rules(): array
     {
-        // dd('chegou aqui');
         return [
             'event' => 'required|array',
             'event.name' => 'required|string|exists:clicksign_events,event',
             'document.key' => 'required|string|exists:documents,document_id',
         ];
     }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Registrando no log para debug
+        Log::error('ClickSign Webhook validation failed.', [
+            'errors' => $validator->errors()->all(),
+            'request' => $this->all()
+        ]);
+
+        return new JsonResponse([
+            'success' => false,
+            'errors'  => ['Unauthorized.'],
+            'data'    => []
+        ], Response::HTTP_OK);
+    }
+
 
     private function validateHmac(): bool
     {
